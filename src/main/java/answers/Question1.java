c90c13f886325486d74f0037d4d40210dea5b5ef
Sam Gao <gaoshan_274@hotmail.com>
2018-10-19 00:36:41 +0100

    did i misunderstand Q1?

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,15 +3,10 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		int ans = 0, v = 0;
+		int ans = 0;
 		for (int i=0;i<portfolios.length;i++) {
 			for(int j=i+1;j<portfolios.length;j++){
-				int vf = portfolios[i] ^ portfolios[j];
-				int vx = Integer.bitCount(vf);
-				if (vx > v) {
-					v = vx;
-					ans = vf;
-				}
+				ans = Math.max(ans, portfolios[i] ^ portfolios[j]);
 			}
 		}
 		return ans;
