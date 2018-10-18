50313525504e400ed9b203b52423649e227bcd4e
Sam Gao <gaoshan_274@hotmail.com>
2018-10-19 00:47:35 +0100

    Q6

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -2,8 +2,41 @@ package answers;
 
 public class Question6 {
 
+	class State {
+		public int cn;
+		public int cd;
+		public State(int n, int d) {
+			cn = n;
+			cd = d;
+		}
+	}
+
+	class Compare implements Comparator<State> {
+		@Override
+		public int compare(State a0, State a1) {
+			if (a0.cd == a1.cd) return 0;
+			else if (a0.cd < a1.cd) return -1;
+			else return 1;
+		}
+	}
+
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-		return -1;	
+		PriorityQueue<State> pq = new PriorityQueue<State>(1, new Compare());
+		Boolean[] v = new Boolean[numServers];
+		pq.add(new State(0, 0));
+		while(pq.size() != 0) {
+			State c = pq.remove();
+			if (c.cn == targetServer) {
+				return c.cd;
+			}
+			if(v[c.cn]) continue;
+			v[c.cn] = true;
+			for(int i=0;i<numServers;i++){
+				if (v[i]) continue;
+				pq.add(new State(i, c.cd + times[c.cn][i]));
+			}
+		}
+		return times[0][targetServer];
 	}
 
 }
