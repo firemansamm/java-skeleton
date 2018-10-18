686b5485e894bf891b1c0538cb1059d080ae8282
Sam Gao <gaoshan_274@hotmail.com>
2018-10-18 21:42:30 +0100

    fix Q5

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -4,7 +4,7 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		int[] ans = new int[totalValue + 1];
-		Arrays.fill(ans, allowedAllocations.length + 5)
+		Arrays.fill(ans, allowedAllocations.length + 5);
 		ans[0] = 0;
 		for(int i=1;i<=totalValue;i++){
 			for(int j=0;j<allowedAllocations.length;j++){
