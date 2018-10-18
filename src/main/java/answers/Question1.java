25502f6551afe1fd06eea2bd5258982e928f1085
Sam Gao <gaoshan_274@hotmail.com>
2018-10-18 21:11:43 +0100

    Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,13 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+		int ans = 0;
+		for (int i=0;i<portfolios.length;i++) {
+			for(int j=i+1;j<portfolios.length;j++){
+				ans = Math.max(ans, Integer.bitCount(portfolios[i] ^ portfolios[j]));
+			}
+		}
+		return ans;
 	}
 
 }
