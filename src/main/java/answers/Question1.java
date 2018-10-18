596e81299359859c2ef8855e4ec8e353845f2193
Sam Gao <gaoshan_274@hotmail.com>
2018-10-18 21:55:28 +0100

    fix Q1

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,10 +3,15 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		int ans = 0;
+		int ans = 0, v = 0;
 		for (int i=0;i<portfolios.length;i++) {
 			for(int j=i+1;j<portfolios.length;j++){
-				ans = Math.max(ans, Integer.bitCount(portfolios[i] ^ portfolios[j]));
+				int vf = portfolios[i] ^ portfolios[j];
+				int vx = Integer.bitCount(vf);
+				if (vx > v) {
+					v = vx;
+					ans = vf;
+				}
 			}
 		}
 		return ans;
