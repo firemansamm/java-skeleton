7bf9a6d9a208f7f45ae23ab07292039f59554151
Sam Gao <gaoshan_274@hotmail.com>
2018-10-21 17:57:40 +0100

    Q3

--- src/main/java/answers/Question3.java
+++ src/main/java/answers/Question3.java
@@ -1,12 +1,45 @@
 package answers;
 
 import helpers.Edge;
+import java.util.ArrayList;
 
 public class Question3 {
 
+	static ArrayList<ArrayList<Integer>> ad;
+	static boolean[] v;
+
+	static int[] dfs(int n, int idx) {
+		int[] ret = new int[]{0, 0};
+		if (v[n]) return ret;
+		int ni = (idx + 1) % 2;
+		for(int o : ad.get(n)) {
+			int[] k = dfs(o, ni);
+			ret[0] += k[0];
+			ret[1] += k[1];
+		}
+		ret[idx]++;
+		return ret;
+	}
+
 	public static int lowestExposureToExchanges(int numNodes, Edge[] edgeList) {
-		// TODO Auto-generated method stub
-		return -1;
+		/* build adjlist */
+		ad = new ArrayList<ArrayList<Integer>>(numNodes + 5);
+		v = new boolean[numNodes + 5];
+		int el = edgeList.length;
+		for(int i=0;i<el;i++){
+			Edge e = edgeList[i];
+			int x = e.edgeA, y = e.edgeB;
+			ad.get(x).add(y);
+			ad.get(y).add(x);
+		}
+		int ans = 0;
+		for(int i=1;i<=numNodes;i++) {
+			if (v[i]) continue;
+			int[] k = dfs(i, 0);
+			if (k[0] > k[1]) ans += k[0] - k[1];
+			else ans += k[1] - k[0];
+		}
+		return ans;
 	}
 
 }
