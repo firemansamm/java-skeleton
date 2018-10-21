142141545752593ba2f999c964aa13b7da66a65f
Sam Gao <gaoshan_274@hotmail.com>
2018-10-21 17:31:50 +0100

    v can be less than 0???

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -5,22 +5,16 @@ import java.util.Arrays;
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		int sgn = totalValue / Math.abs(totalValue);
-		totalValue = Math.abs(totalValue);
+		if (totalValue < 0) throw new IllegalArgumentException("totalValue < 0: " + totalValue.toString());
+
 		int[] ans = new int[totalValue + 1];
 		ans[0] = 0;
-		if (sgn == -1) {
-			for(int i=0;i<allowedAllocations.length;i++){
-				allowedAllocations[i] *= sgn;
-				//if (allowedAllocations[i] < 0) 
-			}
-		}
 		for(int i=1;i<=totalValue;i++){
-			for(int j=0;j<allowedAllocations.length;j++){
+			for(int j=0;j<len;j++){
 				int nx = i - allowedAllocations[j];
 				if (nx < 0) continue;
 				if (ans[i] == 0) ans[i] = ans[nx] + 1;
-				else ans[i] = Math.min(ans[nx] + 1, ans[i]);
+				else if (ans[nx] + 1 < ans[i]) ans[i] = ans[nx] + 1;
 			}
 		}
 		return ans[totalValue];
